715b5a0ee40e9542aa00e016d14dd60e2e7bb176
Paulo Rodrigo <paulorodrigoba@outlook.com>
2024-09-23 02:17:09 -0400

    Atualização de ProfessorService e ProfessorController

--- src/main/java/br/com/cetam/escolar/api/controller/ProfessorController.java
+++ src/main/java/br/com/cetam/escolar/api/controller/ProfessorController.java
@@ -1,5 +1,8 @@
 package br.com.cetam.escolar.api.controller;
 
+import br.com.cetam.escolar.api.domain.professor.DadosCadastroProfessor;
+import br.com.cetam.escolar.api.domain.professor.DadosListaProfessor;
+import br.com.cetam.escolar.api.domain.professor.DadosUpdateProfessor;
 import br.com.cetam.escolar.api.domain.professor.Professor;
 import br.com.cetam.escolar.api.service.ProfessorService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/professores")
@@ -16,14 +20,33 @@ public class ProfessorController {
     private ProfessorService service;
 
     @PostMapping
-    public ResponseEntity<Professor> cadastrarProfessor (@RequestBody Professor professor){
-        Professor novoProfessor = service.cadastrarProfessor(professor);
+    public ResponseEntity<Professor> cadastrarProfessor (@RequestBody DadosCadastroProfessor dados){
+        Professor novoProfessor = service.cadastrarProfessor(dados);
         return new ResponseEntity<>(novoProfessor, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<Professor>> listarProfessor(){
-        List<Professor> professor = service.listarProfessor();
-        return new ResponseEntity<>(professor, HttpStatus.OK);
+    public ResponseEntity<List<DadosListaProfessor>> listarProfessor(){
+        List<DadosListaProfessor> professores = service.listarProfessor();
+        return ResponseEntity.ok(professores);
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Professor> buscarProfessorPorId(@PathVariable Long id){
+        Optional<Professor> professor = service.buscarProfessorPorId(id);
+        return  professor.map(ResponseEntity::ok).orElseGet(()-> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Professor> atualizarProfessor(@PathVariable Long id, @RequestBody DadosUpdateProfessor dadosAtualizados) {
+        Professor professor = service.atualizarProfessor(id, dadosAtualizados);
+        return ResponseEntity.ok(professor);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProfessor(@PathVariable Long id) {
+        service.deletarProfessor(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
--- src/main/java/br/com/cetam/escolar/api/service/ProfessorService.java
+++ src/main/java/br/com/cetam/escolar/api/service/ProfessorService.java
@@ -1,22 +1,56 @@
 package br.com.cetam.escolar.api.service;
 
+import br.com.cetam.escolar.api.domain.endereco.Endereco;
+import br.com.cetam.escolar.api.domain.professor.DadosCadastroProfessor;
+import br.com.cetam.escolar.api.domain.professor.DadosListaProfessor;
+import br.com.cetam.escolar.api.domain.professor.DadosUpdateProfessor;
 import br.com.cetam.escolar.api.domain.professor.Professor;
 import br.com.cetam.escolar.api.repository.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProfessorService {
     @Autowired
     private ProfessorRepository repository;
 
-    public Professor cadastrarProfessor (Professor professor){
+    // Criar professor
+    public Professor cadastrarProfessor(DadosCadastroProfessor dados) {
+        Professor professor = new Professor(dados);
         return repository.save(professor);
     }
 
-    public List<Professor> listarProfessor(){
-        return repository.findAll();
+    // Listar todos os Professores com DadosListaProfessor
+    public List<DadosListaProfessor> listarProfessor() {
+        return repository.findAll().stream()
+                .map(DadosListaProfessor::new)
+                .collect(Collectors.toList());
+    }
+    // Buscar professor por ID
+    public Optional<Professor> buscarProfessorPorId(Long id) {
+        return repository.findById(id);
+    }
+    // Atualizar professor com DadosUpdateProfessor
+    public Professor atualizarProfessor(Long id, DadosUpdateProfessor dados) {
+        Professor professor = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Aluno não encontrado"));
+
+        professor.setCpf(dados.cpf());
+        professor.setNome(dados.nome());
+        professor.setDataNascimento(dados.dataNascimento());
+        professor.setSexo(dados.sexo());
+        professor.setTelefone(dados.telefone());
+        professor.setEndereco(new Endereco(dados.endereco()));
+        professor.setAtivo(true);
+
+        return repository.save(professor);
+    }
+    // Deletar professor
+    public void deletarProfessor(Long id) {
+        repository.deleteById(id);
     }
 }
